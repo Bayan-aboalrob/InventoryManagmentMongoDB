abc000cda56867e30285c950c1a0df93b72a6629
Bayan Aboalrob <bayanaboalrob2001@gmail.com>
2024-07-01 22:09:17 +0300

    Implementing the database context class

---
+++ MongoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using System;
+namespace InventoryManagmentMongoDB
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Product> Products => _database.GetCollection<Product>("Products");
+        public IMongoCollection<InventoryItem> Inventory => _database.GetCollection<InventoryItem>("Inventory");
+    }
+}
