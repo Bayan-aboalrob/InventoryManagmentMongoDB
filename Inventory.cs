6460d3ed73dd5673809f79aceb484b5581a25c31
Bayan Aboalrob <bayanaboalrob2001@gmail.com>
2024-07-01 22:08:20 +0300

    Implementing the Inventory class

---
+++ Inventory.cs
@@ -0,0 +1,32 @@
+﻿namespace InventoryManagmentMongoDB
+{
+    public class Inventory
+    {
+        private Database database;
+
+        public Inventory(string connectionString, string databaseName)
+        {
+            database = new Database(connectionString, databaseName);
+        }
+
+        public void AddOrUpdateInventoryItem(string productName, int quantity)
+        {
+            database.AddOrUpdateInventoryItem(productName, quantity);
+        }
+
+        public void ViewInventory()
+        {
+            database.ViewInventory();
+        }
+
+        public void EditInventoryItem(string inventoryId, int newQuantity)
+        {
+            database.EditInventoryItem(inventoryId, newQuantity);
+        }
+
+        public void DeleteInventoryItem(string inventoryId)
+        {
+            database.DeleteInventoryItem(inventoryId);
+        }
+    }
+}
