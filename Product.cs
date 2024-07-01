29f70eb3cc5fb8c062f3f08959a048e498edaf27
Bayan Aboalrob <bayanaboalrob2001@gmail.com>
2024-07-01 22:07:36 +0300

    Implementing the product class

---
+++ Product.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace InventoryManagmentMongoDB
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"ProductId: {ProductId}, Name: {Name}, Price: {Price:C}");
+        }
+    }
+
+}
