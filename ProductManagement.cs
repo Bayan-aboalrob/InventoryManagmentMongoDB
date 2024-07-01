c3ba91d92c507050d2351ae7b6da3bdf724717a9
Bayan Aboalrob <bayanaboalrob2001@gmail.com>
2024-07-01 22:08:46 +0300

    Implementing product managment class

---
+++ ProductManagement.cs
@@ -0,0 +1,37 @@
+﻿namespace InventoryManagmentMongoDB
+{
+    public class ProductManagement
+    {
+        private Database database;
+
+        public ProductManagement(string connectionString, string databaseName)
+        {
+            database = new Database(connectionString, databaseName);
+        }
+
+        public void AddProduct(Product product)
+        {
+            database.AddProduct(product);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return database.GetAllProducts();
+        }
+
+        public Product FindProductByName(string name)
+        {
+            return database.FindProductByName(name);
+        }
+
+        public Product FindProductById(string productId)
+        {
+            return database.FindProductById(productId);
+        }
+
+        public void DeleteProduct(string productId)
+        {
+            database.DeleteProduct(productId);
+        }
+    }
+}
